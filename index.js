67dade20d5ea9e3871064cd2e12886b167ed577c
GabytaDev <gabyta.loyola@gmail.com>
2021-12-23 16:40:59 -0300

    agrego fetch personajes

---
+++ index.js
@@ -0,0 +1,49 @@
+///ELEMENTOS DEL DOM/////
+const contenedorTarjetas = document.querySelector(".contenedor-tarjetas")
+const formulario = document.querySelector("#formulario")
+const botonBuscar = document.getElementById("buscar")
+const inputBuscador = document.querySelector("#input-buscador")
+const tarjetaPersonaje = document.querySelector(".tarjeta-personaje")
+const selectBusqueda = document.getElementById("select-busqueda")
+
+
+const baseUrl = "https://rickandmortyapi.com/api/"
+
+///FORMULARIO///
+formulario.onsubmit = (event) => {
+    event.preventDefault();
+
+};
+
+botonBuscar.onclick = ()=>{
+   buscador ( selectBusqueda.value, inputBuscador.value)
+
+}
+/** todos los personajes */
+const todosLosPersonajes = ()=>{
+    fetch(`${baseUrl}character`)
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data)
+        mostrarPersonajeEnHTML(data.results)
+    })
+}
+
+todosLosPersonajes()
+
+const mostrarPersonajeEnHTML = (array) => {
+    const html = array.reduce((acc,curr)=>{
+      return acc = acc + 
+      `<div class="card" data-id=${curr.id}>
+       <h3>Nombre: ${curr.name}</h3>
+       <img src="${curr.image}"></img>
+       <p>Genero: ${curr.gender}</p>
+       <p>Especie: ${curr.species}</p>
+     </div>`
+    },"")
+    tarjetaPersonaje.innerHTML = html
+
+   // detalleDePersonaje()
+}  
+
+
